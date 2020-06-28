66b2ba273579029f1aa5d6daec798b60d240c8bc
Marcin Delektowski <marcin.delektowski@gmail.com>
2020-06-28 14:32:48 +0200

    Updates

---
+++ precache-manifest.3907989c21d1e119177101b858a83f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de89adf607af3ecc0a19c38c004d9124",
+    "url": "/iamip/index.html"
+  },
+  {
+    "revision": "e13bdd5477f8756b29ff",
+    "url": "/iamip/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b2138b3645198508541",
+    "url": "/iamip/static/js/2.f99d5f43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/iamip/static/js/2.f99d5f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13bdd5477f8756b29ff",
+    "url": "/iamip/static/js/main.69b8a6f9.chunk.js"
+  },
+  {
+    "revision": "6aab4ff5d3b30403ff01",
+    "url": "/iamip/static/js/runtime-main.3ad791df.js"
+  }
+]);
